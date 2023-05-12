a6b7034d71b8e2821ab32fce2c3d30aae452674a
C3sarCruz <106643035+C3sarCruz@users.noreply.github.com>
2023-05-12 13:56:49 -0600

    Initial commit

---
+++ presupuesto.js
@@ -0,0 +1,102 @@
+function muestraP(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestra_P('contenidoP');
+}
+
+
+function muestraI(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraI('contenidoI');
+}
+
+
+function muestraG(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraG('contenidoG');
+}
+
+
+function muestraGA(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraGA('contenidoGA');
+}
+
+function muestraGM(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraGM('contenidoGM');
+}
+
+function muestraME(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraME('contenidoME');
+}
+
+function muestraA(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraA('contenidoA');
+}
+
+function muestraJ(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraJ('contenidoJ');
+}
+
+function muestraS(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraS('contenidoS');
+}
+
+function muestraE(id) {
+        if (document.getElementById) {
+            var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+        }
+    }
+        window.onload = function () {
+            muestraE('contenidoE');
+}
